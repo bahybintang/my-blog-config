58c7c548ec4dfab9b441499e8e80be6c1066a0e3
Muhammad Bintang Bahy <bahybintang@gmail.com>
2020-04-20 14:41:10 +0700

    update runner and compose

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.staging.yml up -d
-docker-compose up --force-recreate --no-deps certbot
+docker-compose up --no-deps certbot
 docker-compose stop webserver
 docker-compose up -d --force-recreate --no-deps webserver
