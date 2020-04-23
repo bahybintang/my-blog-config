1b368e8d0afcbfae9ef1b66d515b3156f046e653
Muhammad Bintang Bahy <bahybintang@gmail.com>
2020-04-23 05:10:39 +0000

    Move to cloudflare ssl

--- changedomain.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose.staging.yml up -d --force-recreate --no-deps webserver
-docker-compose up -d --force-recreate --no-deps certbot
-docker-compose up -d --force-recreate --no-deps webserver
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose.staging.yml up -d
-docker-compose up --no-deps certbot
-docker-compose stop webserver
-docker-compose up -d --force-recreate --no-deps webserver
